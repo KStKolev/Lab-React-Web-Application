294749d306945e571135cb18cba165ebae5e40f5
Kolyo Kolev <114016354+KStKolev@users.noreply.github.com>
2025-10-01 02:12:01 +0300

    feat: integrate ErrorBoundary and ErrorRoutingPage for enhanced error handling in routing, in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import "./styles/main.scss";
 // watch: native intellisense and file-peek for aliases from jsconfig.json and with none-js files doesn't work: https://github.com/microsoft/TypeScript/issues/29334
 
-import { Component, ErrorInfo /* , StrictMode */ } from "react";
+import { Component /* , StrictMode */ } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import apiEndpoints from "./api.endpoints";
@@ -10,7 +10,11 @@ import Home from "./components/home/home";
 import Products from "./components/products/products";
 import About from "./components/about/about";
 import Footer from "./components/footer/footer";
+import ErrorBoundary from "./components/errorHandler/errorBoundary";
+import ErrorPage from "./components/errorHandler/errorPage";
 import { ROUTES } from "./routes";
+import TestComponent from "./components/testComponent";
+import ErrorRoutingPage from "./components/errorHandler/errorRoutingPage";
 
 interface Props {}
 interface State {}
@@ -37,18 +41,15 @@ class AppContainer extends Component<Props, State> {
     setTimeout(testFetch, 300);
   }
 
-  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    console.error("got err", { error, errorInfo });
-  }
-
   render() {
     return (
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path={ROUTES.HOME} element={<Home />} />
-          <Route path={ROUTES.PRODUCTS} element={<Products />} />
-          <Route path={ROUTES.ABOUT} element={<About />} />
+          <Route path={ROUTES.HOME} element={<Home />} errorElement={<ErrorRoutingPage />} />
+          <Route path={ROUTES.PRODUCTS} element={<Products />} errorElement={<ErrorRoutingPage />} />
+          <Route path={ROUTES.ABOUT} element={<About />} errorElement={<ErrorRoutingPage />} />
+          <Route path="/test" element={<TestComponent />} errorElement={<ErrorRoutingPage />} />
 
           <Route path={ROUTES.CATCH} element={<Navigate to={ROUTES.HOME} replace />} />
         </Routes>
@@ -58,5 +59,9 @@ class AppContainer extends Component<Props, State> {
   }
 }
 
-ReactDOM.createRoot(document.getElementById("app")!).render(<AppContainer />);
+ReactDOM.createRoot(document.getElementById("app")!).render(
+  <ErrorBoundary fallback={ErrorPage}>
+    <AppContainer />
+  </ErrorBoundary>,
+);
 // React + TS: https://github.com/typescript-cheatsheets/react#reacttypescript-cheatsheets
