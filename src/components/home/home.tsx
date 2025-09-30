6a2820bdb35ee8aefcc28ad14afe1c1fc94a0e18
Kolyo Kolev <114016354+KStKolev@users.noreply.github.com>
2025-09-30 20:56:51 +0300

    refactor(Home): change home image to be styled as background

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,9 +2,5 @@ import * as style from "./home.m.scss";
 import backgroundImage from "../../assets/images/background.jpg";
 
 export default function Home() {
-  return (
-    <main>
-      <img src={backgroundImage} className={style.background} alt="Background" />
-    </main>
-  );
+  return <main className={style.background} style={{ backgroundImage: `url(${backgroundImage})` }} />;
 }
